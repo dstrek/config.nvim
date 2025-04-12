561ab89310ccd63922d1d803b688f98a224c3adc
Nathan <batman@dstrek.com>
2025-04-12 22:29:11 +1000

    start options and some keymaps etc

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -1,1 +1,10 @@
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- nnoremap <silent> <leader>q :nohlsearch<CR>
+vim.keymap.set("n", "<leader>q", "<CMD>nohlsearch<CR>", { desc = "Clear search higlighting" })
+
+-- nnoremap <silent> <leader>L :setlocal number!<CR>
+vim.keymap.set("n", "<leader>L", "<CMD>setlocal number!<CR>", { desc = "Line number toggling" })
+
+-- nnoremap <silent> <leader>a :set list!<CR>
+vim.keymap.set("n", "<leader>a", "<CMD>set list!<CR>", { desc = "Toggle render of list chars" })
---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+-- all the tab related things
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- line numbers
+vim.opt.number = true
+
+-- enable mouse mode
+vim.opt.mouse = "a"
+
+-- no need to show since it's in statusline
+vim.opt.showmode = false
+
+-- case insensitive searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- signcolumn ?
+vim.opt.signcolumn = "yes"
+
+-- don't scroll text all the way off screen
+vim.opt.scrolloff = 5
